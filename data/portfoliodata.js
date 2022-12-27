d2a541294a83e93a7750cc29975218a31d75b4bc
Mammadkhan Bayramov <90881574+mammadkhan@users.noreply.github.com>
2022-12-27 15:00:30 +0400

    Multi-step form added

--- data/portfoliodata.js
+++ data/portfoliodata.js
@@ -3,17 +3,21 @@ import azjobit from "../public/websites/azjobit.png";
 import bankapp from "../public/websites/bankapp.png";
 import ecommerce from "../public/websites/ecommerce.png";
 import iplocation from "../public/websites/iplocation.png";
+import multiform from "../public/websites/multiform.png";
 import randomnumfacts from "../public/websites/randomnumfacts.png";
 import sketchapp from "../public/websites/sketchapp.png";
 import snipperapp from "../public/websites/snipperapp.png";
 import weatherapp from "../public/websites/weatherapp.png";
 import dotahol from "../public/websites/dotahol.png";
 
+
 //Import used tool icons
 import react from "../public/tools/react.png";
 import nextjs from "../public/tools/nextjs.png";
 import angular from "../public/tools/angular.png";
 import api from "../public/tools/api.png";
+import vite from "../public/tools/vite.png";
+import redux from "../public/tools/redux.png";
 
 export const portfoliodata = [
 	{
@@ -43,6 +47,13 @@ export const portfoliodata = [
 		domain: "https://mbiplocation.netlify.app",
 		imgUrl: iplocation,
 		tools: [angular, api],
+	},
+	{
+		title: "Multi-step form",
+		domainPreview: "mbmultiform.netlify.app",
+		domain: "http://mbmultiform.netlify.app",
+		imgUrl: multiform,
+		tools: [vite, react, redux],
 	},
 	{
 		title: "Random Number Facts",
